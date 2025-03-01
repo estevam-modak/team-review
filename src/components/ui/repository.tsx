6e231566f692c69e74e24376c783450d464bc465
estevam-modak <estevam.furtado@modak.live>
2025-03-01 13:54:48 -0300

    feat: see last 5 merged

--- src/components/ui/repository.tsx
+++ src/components/ui/repository.tsx
@@ -9,8 +9,8 @@ export function Repository({ id }: { id: string }) {
   const repo = repoMap[id];
 
   return (
-    <div className="w-80 flex-shrink-0 rounded-lg b">
-      <div className="flex flex-row items-start justify-between gap-1 p-1 pb-2">
+    <div className="w-80 flex-shrink-0 rounded-lg b flex flex-col gap-4">
+      <div className="flex flex-row items-start justify-between gap-1 p-1">
         <a
           href={`https://github.com/${repo?.org}/${repo?.name}`}
           target="_blank"
@@ -25,12 +25,13 @@ export function Repository({ id }: { id: string }) {
           <X className="size-4 hover:text-destructive" />
         </button>
       </div>
-      <PRs id={id} />
+      <OpenPRs id={id} />
+      <Last5MergedPRs id={id} />
     </div>
   );
 }
 
-function PRs({ id }: { id: string }) {
+function OpenPRs({ id }: { id: string }) {
   const { data: session } = useSession();
   const { repoMap } = useRepos();
   const token = session
@@ -39,7 +40,7 @@ function PRs({ id }: { id: string }) {
   const repo = repoMap[id];
 
   const {
-    data: prs,
+    data: open,
     isFetching,
     isError,
     dataUpdatedAt,
@@ -60,10 +61,10 @@ function PRs({ id }: { id: string }) {
     ? "Loading..."
     : isError
       ? "Error fetching"
-      : prs
-        ? prs.prs?.length +
+      : open
+        ? open.prs?.length +
           " of " +
-          prs.totalCount +
+          open.totalCount +
           " at " +
           new Date(dataUpdatedAt).toLocaleString("en-UK", {
             hour: "2-digit",
@@ -84,8 +85,66 @@ function PRs({ id }: { id: string }) {
           </button>
         )}
       </div>
-      {prs &&
-        prs.prs.map((pr) => (
+      {open &&
+        open.prs.map((pr) => (
+          <PRCard
+            key={pr.id}
+            pr={pr}
+          />
+        ))}
+    </div>
+  );
+}
+
+function Last5MergedPRs({ id }: { id: string }) {
+  const { data: session } = useSession();
+  const { repoMap } = useRepos();
+  const token = session
+    ? (session as unknown as { accessToken: string })?.accessToken
+    : undefined;
+  const repo = repoMap[id];
+
+  const {
+    data: merged,
+    isFetching,
+    isError,
+    dataUpdatedAt,
+    refetch,
+  } = api.pullRequests.last5MergedPRs.useQuery(
+    {
+      repo: repo?.name || "",
+      org: repo?.org || "",
+      token: token || "",
+    },
+    {
+      retry: false,
+      enabled: repo && !!token && repo.name != "" && repo.org != "",
+    },
+  );
+
+  const text = isFetching
+    ? "Loading..."
+    : isError
+      ? "Error fetching"
+      : merged
+        ? "Last merged"
+        : "Could not fetch";
+
+  return (
+    <div className="flex min-h-24 w-full flex-col gap-3 rounded-sm border bg-muted px-3 py-3">
+      <div className="flex flex-row items-center justify-center gap-2 text-xs text-muted-foreground/50">
+        {text}
+        {!isFetching && (
+          <button
+            className="text-muted-foreground opacity-50 hover:text-ring hover:opacity-100"
+            onClick={() => refetch()}
+          >
+            <RefreshCw className="size-3" />
+          </button>
+        )}
+      </div>
+      {merged &&
+        merged.prs.map((pr) => (
           <PRCard
             key={pr.id}
             pr={pr}
--- src/server/api/routers/pull-requests.ts
+++ src/server/api/routers/pull-requests.ts
@@ -160,4 +160,66 @@ export const pullRequestsRouter = createTRPCRouter({
         totalCount: repository.pullRequests.totalCount,
       };
   }),
+
+  last5MergedPRs: publicProcedure
+    .input(
+      z.object({
+        repo: z.string(),
+        org: z.string(),
+        token: z.string(),
+        page: z.number().optional(),
+      }),
+    )
+    .query(async ({ input }): Promise<{ prs: PullRequest[], totalCount: number }> => {
+      const graphqlWithAuth = graphql.defaults({
+        headers: {
+          authorization: `Bearer ${input.token}`,
+        },
+      });
+
+      const { repository } = await graphqlWithAuth<RepositoryGraphQL>(`
+        query getRepository($owner: String!, $repo: String!, $after: String) {
+          repository(owner: $owner, name: $repo) {
+            pullRequests(states: MERGED, orderBy: {field: CREATED_AT, direction: DESC}, first: 5, after: $after) {
+              totalCount
+              nodes {
+                id
+                number
+                title
+                url
+                state
+                createdAt
+                isDraft
+                author {
+                  login
+                }
+                changedFiles
+                additions
+                deletions
+                latestReviews(first: 100) {
+                  nodes {
+                    id
+                    state
+                    author {
+                      login
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      `, {
+        owner: input.org,
+        repo: input.repo,
+        after: input.page ? `after: "${input.page}"` : undefined,
+      });
+
+      const prs = repository.pullRequests.nodes.map((pr) => mapPullRequest(pr));
+
+      return {
+        prs,
+        totalCount: repository.pullRequests.totalCount,
+      };
+  }),
 });
